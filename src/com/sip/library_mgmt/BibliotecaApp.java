bf678495cf2c224f97ca1717ceedb46590672b63
Megha Agarwal <agarwal.megha7@gmail.com>
2013-07-08 13:48:23 +0530

    set up new project

---
+++ src/com/sip/library_mgmt/BibliotecaApp.java
@@ -0,0 +1,8 @@
+package com.sip.library_mgmt;
+
+public class BibliotecaApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
---
+++ test/com/sip/library_mgmt/ExampleTest.java
@@ -0,0 +1,13 @@
+package com.sip.library_mgmt;
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
